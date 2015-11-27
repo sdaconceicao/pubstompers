b94f56fee90f9e8bfe811bbf529f79cbccb77665
Stephen da Conceicao <daconcei472@gmail.com>
2015-11-27 00:17:59 -0500

    Scroll updates currently active nav item

--- app/modules/nav-menu/nav-menu-directive.js
+++ app/modules/nav-menu/nav-menu-directive.js
@@ -19,13 +19,21 @@ function navMenu($window){
 
                 angular.element($window).bind("scroll", function() {
                     let scrollTop = $(window).scrollTop();
+                    $('.container').each( function() {
+                        let topOfElement = $(this).offset().top,
+                            topView = $(window).scrollTop(),
+                            bottomView = topView + $(window).height();
+                        if( (bottomView >= topOfElement) ){
+                            scope.active = $(this).attr('id');
+                        }
+                    });
+                    scope.$apply();
                     if (scrollTop > navTop) {
                         $(element).find('ul').css({ 'position': 'fixed', 'top':0, 'left':0 });
                     } else {
                         $(element).find('ul').css({ 'position': 'relative' });
                     }
                 });
-
             });
         }
     };
