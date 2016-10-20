f463765263f97e3207bf35f520b5b805c2c15887
Stephen da Conceicao <daconcei472@gmail.com>
2016-10-20 01:02:01 -0400

    Adding new proxy

--- gulp.conf.js
+++ gulp.conf.js
@@ -145,7 +145,7 @@ gulp.task('constants', function(){
             merge: true,
             constants: {
                 api:{
-                    extralifeUrl: '//www.extra-life.org/index.cfm?format=json&fuseaction=',
+                    extralifeUrl: 'https://crossorigin.me/http://www.extra-life.org/index.cfm?format=json&fuseaction=',
                     twitchUrl: 'https://api.twitch.tv/kraken'
                 }
             }
